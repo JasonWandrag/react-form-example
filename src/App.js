7f9f20d35132f31aa4abe1b52f06b4a0cd6de54d
JasonWandrag <jasonwandrag@hotmail.com>
2022-06-25 03:38:26 +0200

    Clean file structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Banner from "./components/banner/Banner";
 import Content from "./components/content/Content";
 import Form from "./components/form/Form";
 
-import { useState, useRef } from "react";
-
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+import { useState } from "react";
 
 function App() {
   const [contentActive, setContentActive] = useState(false);
